a6d7b0e0f2b63d82da9b5fc97b487e9f66bffdd5
bitsapien <bitsapien@gmail.com>
2020-03-02 13:10:15 +0530

    Updates

---
+++ precache-manifest.acf38f75dba562a276e37028371a8c14.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b8c9f4c62ca847d7513accaa6561b4",
+    "url": "/womens-day-interactive/index.html"
+  },
+  {
+    "revision": "1ed911c3f82ce7858d80",
+    "url": "/womens-day-interactive/static/css/main.ed7a5e7e.chunk.css"
+  },
+  {
+    "revision": "1002c1297e3a5f996490",
+    "url": "/womens-day-interactive/static/js/2.4900196a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/womens-day-interactive/static/js/2.4900196a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed911c3f82ce7858d80",
+    "url": "/womens-day-interactive/static/js/main.f9320d19.chunk.js"
+  },
+  {
+    "revision": "457f5024239819612a2f",
+    "url": "/womens-day-interactive/static/js/runtime-main.d961ec2f.js"
+  },
+  {
+    "revision": "d71f7e3cd4ac14e80ac74154d14edc81",
+    "url": "/womens-day-interactive/static/media/cause.d71f7e3c.png"
+  },
+  {
+    "revision": "ed5fa6f96f8aa892fec5b346bcaf231c",
+    "url": "/womens-day-interactive/static/media/educationicon.ed5fa6f9.png"
+  },
+  {
+    "revision": "3f6973450bff6f69912bdb910a4f3392",
+    "url": "/womens-day-interactive/static/media/face.3f697345.png"
+  },
+  {
+    "revision": "338ab1f174fc231c06053b8b50c6b823",
+    "url": "/womens-day-interactive/static/media/faceElated.338ab1f1.png"
+  },
+  {
+    "revision": "01fe674388780cf58427d883eaad04a2",
+    "url": "/womens-day-interactive/static/media/faceHappy.01fe6743.png"
+  },
+  {
+    "revision": "4c2b5b1700de3a2f860f670fbf17e99f",
+    "url": "/womens-day-interactive/static/media/faceSad.4c2b5b17.png"
+  },
+  {
+    "revision": "166fe695a277c9afb0301f69262aeff9",
+    "url": "/womens-day-interactive/static/media/girl-education.166fe695.png"
+  },
+  {
+    "revision": "c2b973638353a40b1601fc6d4c501eed",
+    "url": "/womens-day-interactive/static/media/pops.c2b97363.jpg"
+  },
+  {
+    "revision": "f2515e10b76af5a812547f07351e8fc1",
+    "url": "/womens-day-interactive/static/media/safety.f2515e10.png"
+  },
+  {
+    "revision": "b0db8d840351283c8fe4ebe0bd8d29a6",
+    "url": "/womens-day-interactive/static/media/safetyicon.b0db8d84.png"
+  },
+  {
+    "revision": "48d846a25c544aa46ebabf62ff7f5a04",
+    "url": "/womens-day-interactive/static/media/stereotype.48d846a2.jpg"
+  },
+  {
+    "revision": "e98437eb427c0008c539c3359715231f",
+    "url": "/womens-day-interactive/static/media/stereotype.e98437eb.png"
+  }
+]);
